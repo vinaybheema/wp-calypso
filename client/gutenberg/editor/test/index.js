e4a1dcb8f82e024a379c1c7ce300c77a465f0ac7
Marko Andrijasevic <markoandrijasevic@gmail.com>
2018-08-01 01:58:50 +0200

    Gutenberg: import @wodpress/block-serialization-spec-parser package

--- client/gutenberg/editor/test/index.js
+++ client/gutenberg/editor/test/index.js
@@ -21,6 +21,8 @@ import {
 
 import { createBlock, registerBlockType } from '@wordpress/blocks';
 
+import { parse } from '@wordpress/block-serialization-spec-parser';
+
 describe( 'Gutenberg @wordress/data package', () => {
 	describe( 'registerStore', () => {
 		it( 'should be shorthand for reducer, actions, selectors registration', () => {
@@ -146,5 +148,23 @@ describe( 'Gutenberg @wordress/blocks package', () => {
 			expect( block.innerBlocks[ 0 ].name ).toBe( 'core/test-block' );
 			expect( typeof block.clientId ).toBe( 'string' );
 		} );
+	} );
+} );
+
+describe( 'block-serialization-spec-parser', () => {
+	// Try test from https://github.com/WordPress/gutenberg/blob/54990c0f77294e6603cc4e0aa957c0679c1bf242/packages/block-serialization-spec-parser/test/index.js
+	test( 'parse() works properly', () => {
+		const result = parse( '<!-- wp:core/more --><!--more--><!-- /wp:core/more -->' );
+
+		expect( result ).toMatchInlineSnapshot( `
+Array [
+  Object {
+    "attrs": null,
+    "blockName": "core/more",
+    "innerBlocks": Array [],
+    "innerHTML": "<!--more-->",
+  },
+]
+` );
 	} );
 } );
